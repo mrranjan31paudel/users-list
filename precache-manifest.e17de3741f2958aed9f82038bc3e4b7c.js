70bb2f5d67f09564eee6b713969380afcb4b6246
mrranjan31paudel <paudeljimrranjan@gmail.com>
2020-01-08 17:05:42 +0545

    Updates

---
+++ precache-manifest.e17de3741f2958aed9f82038bc3e4b7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad9c4ed373913d870033acf59fc26fa",
+    "url": "/users-list/index.html"
+  },
+  {
+    "revision": "9bd24f6a74e22084875f",
+    "url": "/users-list/static/css/main.9376936c.chunk.css"
+  },
+  {
+    "revision": "d56222dde41ba5a01445",
+    "url": "/users-list/static/js/2.8e3efda0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/users-list/static/js/2.8e3efda0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9bd24f6a74e22084875f",
+    "url": "/users-list/static/js/main.17f2c5e7.chunk.js"
+  },
+  {
+    "revision": "166e2f1f22e3ad53533a",
+    "url": "/users-list/static/js/runtime-main.8e34cba3.js"
+  }
+]);
